8dd226cc2e00bf8042d180bcb64ea71cdf76eca5
heshan <zq2383306981@163.com>
2022-07-29 19:33:42 +0800

    7.29

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    printf("text");
+int* array_create(int);
+void array_free(int*);
 
+int main(){
+    
+    int n = 15; 
+    int* array;
+    
+    printf("请输入数组内单元个数\n");
+    scanf("%d",&n);
+    array = array_create(n);
+    // for (int i = 0; i < n; i++){
+    //  printf("%d.\tarray[%d] = %d\n",i+1,i,array[i]);
+    // }
+    array_free(array);
+    
     return 0;
+}
+
+
+int* array_create(int array_num){
+
+    int* array_p = (int*)malloc(sizeof(int)*array_num);
+
+    for ( int i = 0; i < array_num; i++){
+        array_p[i] = i;
+    }
+
+    return(array_p);
+}
+
+void array_free(int* array_p){
+
+    free(array_p);
 }
